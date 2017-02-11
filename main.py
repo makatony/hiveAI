f4619607d7b46e28b8fd9feb3e511248918da94a
Martin Ockajak <m@hq.sk>
2017-02-11 15:14:20 +0100

    Added player pieces search

--- main.py
+++ main.py
@@ -5,8 +5,17 @@ from state import Piece
 
 
 # Entry point
-board = state.Board({ (0, 0): Piece(state.ANT, 0) })
+board = state.Board({
+    (0, 0): Piece(state.ANT, 0),
+    (0, 1): Piece(state.ANT, 1)
+})
 print(board)
-print(list(board.my_neighbours((0, 0))))
-print(board.new_placements())
+print('My pieces: ' + str(list(board.my_pieces())))
+print('Opponent pieces: ' + str(list(board.opponent_pieces())))
+print('Occupied neighours: ' + str(list(board.occupied_neighbours((0, 0)))))
+print('Empty neighours: ' + str(list(board.empty_neighbours((0, 0)))))
+print('My neighours: ' + str(list(board.my_neighbours((0, 0)))))
+print('Opponent neighours: ' + str(list(board.opponent_neighbours((0, 0)))))
+print('Connected placements: ' +str( board.connected_placements()))
+print('New placements: ' + str(board.new_placements()))
 
--- state.py
+++ state.py
@@ -17,7 +17,7 @@ class Board:
 
         # map of position: (x,y) -> Piece
         self.positions = positions
-        for (position, piece) in positions.items():
+        for position, piece in positions.items():
             self.stack[piece.player][piece.insect] = max(self.stack[piece.player][piece.insect] - 1, 0)
 
         # next player number
@@ -33,31 +33,37 @@ class Board:
     def new_placements(self):
         """Valid placements for a new piece. These are connected placements not neighbouring any opponent's pieces."""
         opponent_connected = set()
-        for (position, piece) in self.positions.items():
+        for position, piece in self.positions.items():
             if piece.player != self.next_player:
                 opponent_connected.update(self.empty_neighbours(position))
         return self.connected_placements() - opponent_connected
 
+    def my_pieces(self):
+        return (position for position, piece in self.positions.items() if piece.player == self.next_player)
+
+    def opponent_pieces(self):
+        return (position for position, piece in self.positions.items() if piece.player != self.next_player)
+
     def my_neighbours(self, position):
         """Neighbouring positions with next player's piece"""
-        return self.neighbours(position, self.next_player)
+        return (pos for pos in self.occupied_neighbours(position) if self.positions[pos].player == self.next_player)
 
     def opponent_neighbours(self, position):
         """Neighbouring positions with opponent's piece"""
-        return (p for p in self.neighbours(position) if self.positions(p).player != player)
+        return (pos for pos in self.occupied_neighbours(position) if self.positions[pos].player != self.next_player)
+
+    def occupied_neighbours(self, position):
+        """Neighbouring positions with a piece"""
+        return (pos for pos in self.neighbours(position) if pos in self.positions)
 
     def empty_neighbours(self, position):
         """Neighbouring positions without a piece"""
-        return (p for p in self.directions(position) if p not in self.positions)
+        return (pos for pos in self.neighbours(position) if pos not in self.positions)
 
-    def neighbours(self, position, player):
-        """Neighbouring positions for specified player"""
-        return (p for p in self.directions(position) if p in self.positions and self.positions(p).player == player)
-
-    def directions(self, position):
+    def neighbours(self, position):
         """Valid neighbouring positions"""
         (x, y) = position
-        return [(x, y - 1), (x - 1, y + 1), (x, y + 1), (x - 1, y), (x - 1, y), (x - 1, y - 1)]
+        return [(x, y - 1), (x + 1, y - 1), (x + 1, y), (x, y + 1), (x - 1, y), (x - 1, y - 1)]
 
     def __str__(self):
         return "not implemented"
