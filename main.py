a99856ba0f752854d08badb4b72b232dbc602565
Martin Ockajak <m@hq.sk>
2017-02-11 14:04:41 +0100

    Added neighbours search primitives Added board state initialization

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 import state
+from state import Piece
 
 
 # Entry point
-board = state.Board()
+board = state.Board({ (0, 0): Piece(state.ANT, 0) })
 print(board)
+print(list(board.my_neighbours((0, 0))))
 
--- state.py
+++ state.py
@@ -5,16 +5,39 @@ GRASSHOPPER = "G"
 QUEEN = "Q"
 SPIDER = "S"
 
+# Number of players
+PLAYERS = 2
+
+
 class Board:
-    def __init__(self):
+    def __init__(self, positions = {}):
+        # stack of available pieces: player number -> (insect -> count)
+        self.stack = dict((player, { ANT: 3, BEETLE: 2, GRASSHOPPER: 3, QUEEN: 1, SPIDER: 2 }) for player in range(PLAYERS))
+
         # map of position: (x,y) -> Piece
-        self.positions = {}
+        self.positions = positions
+        for (position, piece) in positions.items():
+            self.stack[piece.player][piece.insect] = max(self.stack[piece.player][piece.insect] - 1, 0)
 
         # next player number
-        self.next = 0
+        self.next_player = 0
 
-        # stack of available pieces: player number -> (insect -> count)
-        self.stack = dict((player, { ANT: 3, BEETLE: 2, GRASSHOPPER: 3, QUEEN: 1, SPIDER: 2 }) for player in range(2))
+    def neighbours(self, position, player):
+        """Neighbouring pieces for specified position and player"""
+        return (p for p in self.directions(position) if p in self.positions and self.positions(p).player == player)
+
+    def my_neighbours(self, position):
+        """Neighbouring pieces owned by the next player for specified position"""
+        return self.neighbours(position, self.next_player)
+
+    def opponent_neighbours(self, position):
+        """Neighbouring pieces owned by the opponents for specified position"""
+        return (p for p in self.neighbours(position) if self.positions(p).player != player)
+
+    def directions(self, position):
+        """Valid neighbouring positions"""
+        (x, y) = position
+        return [(x, y - 1), (x - 1, y + 1), (x, y + 1), (x - 1, y), (x - 1, y), (x - 1, y - 1)]
 
     def __str__(self):
         return "not implemented"
